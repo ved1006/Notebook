f98a01faedc3830e7fc329dda246fa44d7598384
Ved Dange <dangeved106@gmail.com>
2026-01-17 19:08:59 +0530

    Time: 39 ms (8.07%), Space: 47 MB (42.51%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)) {
+                map.put(ch,map.get(ch) +1);
+            } else {
+                map.put(ch,1);
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            char ch = s.charAt(i);
+            if(map.get(ch) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
